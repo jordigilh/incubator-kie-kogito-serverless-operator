c56a6daddf4bb35162f8796452ff25599145226d
Jordi Gil <jgil@redhat.com>
2023-11-09 16:01:54 -0500

    fix build

---
+++ vendor/github.com/apache/incubator-kie-kogito-serverless-operator/container-builder/builder/kubernetes/action.go
@@ -0,0 +1,44 @@
+// Copyright 2022 Red Hat, Inc. and/or its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kubernetes
+
+import (
+	"context"
+
+	"github.com/apache/incubator-kie-kogito-serverless-operator/container-builder/api"
+	"github.com/apache/incubator-kie-kogito-serverless-operator/container-builder/client"
+)
+
+type Action interface {
+	client.Injectable
+	// Name returns user-friendly name for the action
+	Name() string
+
+	// CanHandle returns true if the action can handle the build
+	CanHandle(build *api.ContainerBuild) bool
+
+	// Handle executes the handling function
+	Handle(ctx context.Context, build *api.ContainerBuild) (*api.ContainerBuild, error)
+}
+
+type baseAction struct {
+	client client.Client
+}
+
+// TODO: implement our client wrapper
+
+func (action *baseAction) InjectClient(client client.Client) {
+	action.client = client
+}
